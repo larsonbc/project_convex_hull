9bd3018f1baf7ee3dd45e0853d3c9948247690f0
larsonbc <bchaselarson@gmail.com>
2025-10-21 16:31:20 -0600

    Init

---
+++ convex_hull.py
@@ -0,0 +1,13 @@
+# Uncomment this line to import some functions that can help
+# you debug your algorithm
+# from plotting import draw_line, draw_hull, circle_point
+
+
+def compute_hull_dvcq(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
+    """Return the subset of provided points that define the convex hull"""
+    return []
+
+
+def compute_hull_other(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
+    """Return the subset of provided points that define the convex hull"""
+    return []
